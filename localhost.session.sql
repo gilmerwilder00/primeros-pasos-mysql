cbe27211bfd2e0a10a1388e02362d1f90e77b193
gilmerwilder00 <gilmerwilder00@gmail.com>
2024-09-12 20:59:29 -0500

    comando select

--- localhost.session.sql
+++ localhost.session.sql
@@ -151,7 +151,88 @@ MODIFY column monto_total decimal(10,2) generated always as (cantidad * precio_u
 
 insert into ventas(cliente_id, producto_id, cantidad, empleado_id, precio_unitario) values (1, 1, 2, 1, 1200.00);
 
+--DML - Continuando con la práctica
+
+--1. Inserta un nuevo producto en la tabla "productos" con el nombre "Teléfono móvil" y un precio de 450.00.
+
+insert into productos(nombre, precio) values('Teléfono móvil', 450.00);
+
+--2. Inserta un nuevo cliente en la tabla "clientes" con el nombre "María García" y la dirección "Constitución 456, Luján".
+
+insert into clientes(nombre, direccion) values ('María García', 'Constitución 456, Luján');
+
+--3. Modifica la columna correo_electronico de la tabla empleados para que se genere automáticamente concatenado el nombre, apellido y el string “@mail.com”.
+
+alter table empleados
+MODIFY correo_electronico  varchar(100)
+generated always as (concat(nombre, apellido, '@mail.com')) stored;
+
+--4. Inserta un nuevo empleado en la tabla "empleados" con el nombre "Luis” y apellido “Fernández", edad 28, salario 2800.00 y que pertenezca al departamento “ventas”.
+
+insert into empleados(nombre, apellido, edad, salario, departamento_id) 
+values ('Luis', 'Fernández', 28, 2800.00, (select id from departamentos where nombre='Ventas'));
+
+--5. Actualiza el precio del producto "Laptop" a 1350.00 en la tabla "productos".
+-- select precio FROM productos where nombre = 'Laptop';
+
+update productos set precio =  1350.00 where nombre ='Laptop';
+
+--6. Modifica la dirección del cliente "Juan Pérez" a "Alberti 1789, Mar del Plata" en la tabla "clientes".
+
+update clientes set direccion = 'Alberti 1789, Mar del Plata' where nombre = 'Juan Pérez';
+
+--7. Incrementa el salario de todos los empleados en un 5% en la tabla "empleados".
+
+update empleados set salario = salario * 1.05 ;
+
+--8. Inserta un nuevo producto en la tabla "productos" con el nombre "Tablet" y un precio de 350.00.
+
+insert into productos(nombre, precio) values ('Tablet', 350.00);
+
+--9. Inserta un nuevo cliente en la tabla "clientes" con el nombre "Ana López" y la dirección "Beltrán 1452, Godoy Cruz".
+
+insert into clientes(nombre, direccion) values ('Ana López', 'Beltrán 1452, Godoy Cruz');
+
+
+--10. Inserta un nuevo empleado en la tabla "empleados" con el nombre "Marta", apellido "Ramírez", edad 32, salario 3100.00 y que pertenezca al departamento “ventas”.
+
+
+insert into empleados(nombre, apellido, edad, salario, departamento_id) 
+values ('Martha', 'Ramírez', 32, 3100.00, (select id from departamentos where nombre='Ventas'));
+
+--11. Actualiza el precio del producto "Teléfono móvil" a 480.00 en la tabla "productos".
+update productos set precio = 480.00 where nombre = 'Teléfono móvil';
+
+--12. Modifica la dirección del cliente "María García" a "Avenida 789, Ciudad del Este" en la tabla "clientes".
+
+update clientes set direccion = 'Avenida 789, Ciudad del Este' where nombre = 'María García';
+
+--13. Incrementa el salario de todos los empleados en el departamento de "Ventas" en un 7% en la tabla "empleados".
+
+update empleados set salario = salario*1.07 
+where departamento_id = (select id from departamentos where nombre = 'Ventas');
+
+--14. Inserta un nuevo producto en la tabla "productos" con el nombre "Impresora" y un precio de 280.00.
+
+insert into productos(nombre, precio) values ('Impresora', 280.00);
+
+--15. Inserta un nuevo cliente en la tabla "clientes" con el nombre "Carlos Sánchez" y la dirección "Saavedra 206, Las Heras".
+
+insert into clientes(nombre, direccion) values ('Carlos Sánchez', 'Saavedra 206, Las Heras' );
+
+--16. Inserta un nuevo empleado en la tabla "empleados" con el nombre "Lorena", apellido "Guzmán", edad 26, salario 2600.00 y que pertenezca al departamento “ventas”.
+
+insert into empleados(nombre, apellido, edad, salario, departamento_id) 
+values ('Lorena', 'Guzmán', 26, 2600.00, (select id from departamentos where nombre='Ventas'));
+
+--17. Haz una consulta simple de los datos de la tabla empleados y verifica que se presente de la siguiente manera:
+
 select * from empleados;
-select * from productos;
+
+--18. Haz una consulta simple de los datos de la tabla clientes y verifica que se presente de la siguiente manera:
+
 select * from clientes;
-select * from ventas;
+
+--19. Haz una consulta simple de los datos de la tabla productos y verifica que se presente de la siguiente manera:
+
+select * from productos;
